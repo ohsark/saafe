a3216c2fae0e341d61fe23dd40cc0a6ec476937b
sahil5695 <sahil5695@gmail.com>
2025-05-19 00:17:48 +1000

    Build updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/saafe/', 
-  plugins: [react()]
-})
+  base: '/saafe/',         // ✅ VERY IMPORTANT
+  plugins: [react()],
+});
